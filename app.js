0e46098a0f9e80da1e4875e4e7261c942b1e9268
Messyas <messyasgoisfranca@gmail.com>
2025-04-17 09:11:21 -0400

    Usando fs

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-const http = require('http') //importacao em tempo de execucao de forma sincrona.
+//const http = require('http') -> importacao em tempo de execucao de forma sincrona.
 //ja importacao vai import vai ser em tempo de compilacao e de forma asincrona.  
+import http from 'http'; //padrao module
+import fs from 'fs';
 
-const servidor = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain; charset=utf-8');
-    res.end('Ola, Tic em Trilhas');
+//metodos sincronos, podem gerar problemas de performace. ( writeFileSync, readFileSync)
+fs.writeFile('./mensagem.txt', 'Ola, TIC em Trilhas do arquivo!', 'utf-8', (erro) => {
+    if (erro) {
+        console.log('Falha ao escrever o arquivo', erro);
+        return;
+    }
+    console.log('Arquivo foi criado con sucesso');
 });
 
-const porta = 3000;
-const host = 'localhost';
-
-servidor.listen(porta, host, () => {
-    console.log(`Servidor executando em http://${host}:${porta}/`);
+fs.readFile('./mensagem.txt', 'utf-8', (erro, conteudo) => {
+    if (erro) {
+        console.log('Falha na leitura do arquivo', erro);
+        return;
+    }
+    console.log(`Conteudo: ${conteudo}`);
+    iniciaServidorHttp(conteudo);
 });
 
+function iniciaServidorHttp(mensagem) {
+    const servidor = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-type', 'text/plain; charset=utf-8');
+        res.end(mensagem);
+    });
+
+    const porta = 3000;
+    const host = 'localhost';
+
+    servidor.listen(porta, host, () => {
+        console.log(`Servidor executando em http://${host}:${porta}/`);
+    });
+}
+
+/* ============================================================================|
 exemploTradicional();
 
 //util para acessar a funcao antes da declaracao
@@ -32,3 +55,4 @@ const exemploExpressao = function() {
 const exemploArrow = () => {
     console.log('Arrow');
 };
+*/
